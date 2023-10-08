c4e87aea4fb8ce081458a19ad27a1cee9a633dcb
Mag1strVark <molot2101@gmail.com>
2023-10-08 23:28:23 +0500

    add routes and axios interceptor

---
+++ src/router/index.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { authRoutes, publicRoutes } from './routes.ts'
+
+const AppRouter = () => {
+  const location = useLocation()
+  return (
+    <Routes location={location}>
+      {authRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+    </Routes>
+  )
+}
+
+export default AppRouter
---
+++ src/router/routes.ts
@@ -0,0 +1,9 @@
+import { MAIN_ROUTE } from '../utils/constsRoutes.ts'
+export const authRoutes = [
+  {
+    path: MAIN_ROUTE,
+    // Component: <Main/>
+  },
+]
+
+export const publicRoutes = []
