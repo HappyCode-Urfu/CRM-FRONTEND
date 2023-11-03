08022f4f7694f48905c63d601c2f8761a08c4d0d
Mag1strVark <molot2101@gmail.com>
2023-11-03 15:54:10 +0500

    add styles in navbar

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,4 +1,13 @@
 import s from './Navbar.module.scss'
+import { NavLink } from 'react-router-dom'
+import { CABINET_ROUTE } from 'utils/constsRoutes.ts'
+
 export const Navbar = () => {
-  return <div className={s.container}></div>
+  return (
+    <div className={s.container}>
+      <div className={s.cabinet}>
+        <NavLink to={CABINET_ROUTE}>Личный кабинет</NavLink>
+      </div>
+    </div>
+  )
 }
