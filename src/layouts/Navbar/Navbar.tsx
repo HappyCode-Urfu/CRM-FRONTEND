8b30e4fda8029ae3dfba2e70d8ce1ef5af4131e4
Mag1strVark <molot2101@gmail.com>
2023-11-08 17:31:58 +0500

    add navigate

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,15 +1,15 @@
 import s from './Navbar.module.scss'
-import { NavLink } from 'react-router-dom'
 import { CABINET_ROUTE } from 'utils/constsRoutes.ts'
 import { MiniCalendar } from 'components/miniCalendar/MiniCalendar.tsx'
 import { useState } from 'react'
+import { NavButton } from 'components/cabinet-module/UI/NavButton/NavButton.tsx'
 
 export const Navbar = () => {
   const [selectedDate, setSelectedDay] = useState(new Date())
   return (
     <div className={s.container}>
       <div className={s.cabinet}>
-        <NavLink to={CABINET_ROUTE}>Личный кабинет</NavLink>
+        <NavButton route={CABINET_ROUTE} children={'Личный кабинет'} />
       </div>
       <MiniCalendar
         selectedDate={selectedDate}
