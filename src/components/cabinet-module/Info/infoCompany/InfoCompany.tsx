01781f8de3580380ba2c5ab3a73aa4083d88e714
Mag1strVark <molot2101@gmail.com>
2023-11-12 17:05:36 +0500

    add styles in Cabinet

--- src/components/cabinet-module/Info/infoCompany/InfoCompany.tsx
+++ src/components/cabinet-module/Info/infoCompany/InfoCompany.tsx
@@ -5,13 +5,13 @@ import { COMPANIES_ROUTE } from 'utils/constsRoutes.ts'
 export const InfoCompany = () => {
   return (
     <div className={s.container}>
-      <div className={s.title}>Ваши филиалы</div>
+      <div className={s.title}>
+        <p>Ваши филиалы</p>
+        <NavButton route={COMPANIES_ROUTE} children={'Создать филиал'} />
+      </div>
       <div className={s.block}>
         <div className={s.infoCompany}>
           <p>Cписок филиалов:</p>
-        </div>
-        <div className={s.route}>
-          <NavButton route={COMPANIES_ROUTE} children={'Создать филиал'} />
         </div>
       </div>
     </div>
