c1e34c90c7ee90a6c5ae3ff0de9e46c8c8cfafb5
Mag1strVark <molot2101@gmail.com>
2023-11-14 17:30:16 +0500

    add style

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,11 +1,11 @@
 import s from './Navbar.module.scss'
 import { CABINET_ROUTE } from 'utils/constsRoutes.ts'
 import { MiniCalendar } from 'components/miniCalendar/MiniCalendar.tsx'
-import { NavButton } from 'components/UI/NavButton/NavButton.tsx'
 import { useAppDispatch, useAppSelector } from 'hooks/redux.ts'
 import { eventSlice } from 'store/reducers/Events/EventSlice.ts'
 import { WidgetsButton } from 'components/UI/WidgetsButton/WidgetsButton.tsx'
 import { Button } from 'components/UI/Button/Button.tsx'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   const { dateSelect } = useAppSelector((state) => state.eventReducer)
@@ -15,7 +15,11 @@ export const Navbar = () => {
   return (
     <div className={s.container}>
       <div className={s.cabinet}>
-        <NavButton route={CABINET_ROUTE} children={'Личный кабинет'} />
+        <NavLink
+          className={s.buttonCabinet}
+          to={CABINET_ROUTE}
+          children={'Личный кабинет'}
+        />
       </div>
       <MiniCalendar
         selectedDate={dateSelect}
