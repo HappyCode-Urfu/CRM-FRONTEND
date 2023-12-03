d8650f57ce8bc0db4d1899e2be2344c4d0f6f421
Mag1strVark <molot2101@gmail.com>
2023-12-03 14:16:29 +0500

    edit infocompany

--- src/components/cabinet-module/Info/infoCompany/InfoCompany.tsx
+++ src/components/cabinet-module/Info/infoCompany/InfoCompany.tsx
@@ -4,34 +4,43 @@ import { COMPANIES_ROUTE } from 'utils/constsRoutes.ts'
 import { useTypedDispatch, useTypedSelector } from 'hooks/redux.ts'
 import { useEffect } from 'react'
 import { getDepartment } from 'store/reducers/Departaments/DepartmentActionCreators.ts'
+import { Loading } from 'components/loading/Loading.tsx'
 
 export const InfoCompany = () => {
   const dispatch = useTypedDispatch()
-  const { data } = useTypedSelector((state) => state.departmentReducer)
+  const { data, isLoading, error } = useTypedSelector((state) => state.departmentReducer)
 
   useEffect(() => {
     dispatch(getDepartment())
   }, [])
 
   return (
-    <div className={s.container}>
-      <div className={s.title}>Ваши филиалы</div>
-      <div className={s.block}>
-        <div className={s.infoCompany}>
-          <p>Cписок филиалов:</p>
-        </div>
-        <div className={s.companyList}>
-          {data.map((res, index) => (
-            <div>
-              <p>{index + 1}</p>
-              <p>{res.name}</p>
+    <>
+      {isLoading && <Loading />}
+      {error && <h1>{error}</h1>}
+      {!isLoading && !error && (
+        <>
+          <div className={s.container}>
+            <div className={s.title}>Ваши филиалы</div>
+            <div className={s.block}>
+              <div className={s.infoCompany}>
+                <p>Cписок филиалов:</p>
+              </div>
+              <div className={s.companyList}>
+                {data.map((res, index) => (
+                  <div>
+                    <p>{index + 1}</p>
+                    <p>{res.name}</p>
+                  </div>
+                ))}
+              </div>
+              <div className={s.route}>
+                <NavButton route={COMPANIES_ROUTE} children={'Создать филиал'} />
+              </div>
             </div>
-          ))}
-        </div>
-        <div className={s.route}>
-          <NavButton route={COMPANIES_ROUTE} children={'Создать филиал'} />
-        </div>
-      </div>
-    </div>
+          </div>
+        </>
+      )}
+    </>
   )
 }
